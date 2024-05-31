b0b3eb71ec5308b5525166bec4016cd1812c92b5
Faisal Bahari <fbhr1112@gmail.com>
2024-05-31 16:20:59 +0700

    fix: response api

--- app.py
+++ app.py
@@ -8,8 +8,24 @@ import uuid
 app = Flask(__name__)
 model = YOLO('./model.pt')
 
-SAVE_DIR = 'tmp'
-os.makedirs(SAVE_DIR, exist_ok=True)
+IMAGE_DIR = 'images'
+TEXT_DIR = 'texts'
+os.makedirs(IMAGE_DIR, exist_ok=True)
+os.makedirs(TEXT_DIR, exist_ok=True)
+
+def parse_result_to_json(result_txt_path):
+    with open(result_txt_path, 'r') as file:
+        lines = file.readlines()
+    
+    results = []
+    for line in lines:
+        parts = line.strip().split()
+        if len(parts) >= 2:
+            probability = parts[0]
+            label = ' '.join(parts[1:])
+            results.append({'label': label, 'probability': float(probability)})
+    
+    return results
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -24,17 +40,18 @@ def predict():
         image = Image.open(io.BytesIO(file.read()))
         results = model(image)
 
-        unique_filename = str(uuid.uuid4()) + '.jpg'
-        result_image_path = os.path.join(SAVE_DIR, unique_filename)
-
+        unique_image_filename = str(uuid.uuid4()) + '.jpg'
+        result_image_path = os.path.join(IMAGE_DIR, unique_image_filename)
         result = results[0]
         result.save(result_image_path)
 
-        # return img file
-        # return send_file(result_image_path, mimetype='image/jpeg')
+        unique_text_filename = str(uuid.uuid4()) + '.txt'
+        result_txt_path = os.path.join(TEXT_DIR, unique_text_filename)
+        result.save_txt(result_txt_path)
+
+        prediction_results = parse_result_to_json(result_txt_path)
         
-        # return img path
-        return jsonify({'image_path': unique_filename})
+        return jsonify({'image_path': unique_image_filename, 'predictions': prediction_results})
 
     except Exception as e:
         return jsonify({'error': str(e)}), 500
@@ -43,7 +60,7 @@ def predict():
 @app.route('/results/<filename>', methods=['GET'])
 def get_image(filename):
     try:
-        return send_from_directory(SAVE_DIR, filename)
+        return send_from_directory(IMAGE_DIR, filename)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
